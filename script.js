63bd33598af225ede41fb40b4dacb0a79adf99c5
Manuela <manuella.m.p182@gmail.com>
2022-08-22 15:40:18 -0300

    versao com distribuicao atualizada

--- script.js
+++ script.js
@@ -18,4 +18,44 @@ function QntdDeCartas() {
 
 QntdDeCartas();
 
+let jogo = [];
+let contador = 0;
+
+function IniciarJogo() {
+  icons.sort(comparador);
+
+  while (jogo.length < numCartas) {
+    jogo.push(
+      `<div class="carta" onclick="cliqueNaCarta(this)">
+            <div class="carta-frente"><img src="imagens/front.png"/></div>
+            <div class="carta-tras escondido"><img src="${icons[contador]}"/></div>
+            </div>`,
+      `<div class="carta" onclick="cliqueNaCarta(this)">
+            <div class="carta-frente"><img src="imagens/front.png"/></div>
+            <div class="carta-tras escondido"><img src="${icons[contador]}"/></div>
+            </div>`
+    );
+    contador += 1;
+  }
+
+  jogo.sort(comparador);
+}
+
+console.log(jogo);
+
+function comparador() {
+  return Math.random() - 0.5;
+}
+
+IniciarJogo();
+
+function BaralhoNaTela() {
+  for (let contador = 0; contador < jogo.length; contador++) {
+    const baralho = document.querySelector(".distribuicao-de-cartas");
+    baralho.innerHTML += jogo[contador];
+  }
+}
+
+BaralhoNaTela();
+
 
