d4f116ad1085b19f8b694e872c067e19542d6845
Manuela <manuella.m.p182@gmail.com>
2022-08-22 15:38:57 -0300

    versao com prompt

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let numCartas;
 
+let icons = [
+  `imagens/bobrossparrot.gif`,
+  `imagens/explodyparrot.gif`,
+  `imagens/fiestaparrot.gif`,
+  `imagens/metalparrot.gif`,
+  `imagens/revertitparrot.gif`,
+  `imagens/unicornparrot.gif`,
+  `imagens/tripletsparrot.gif`
+];
+
+function QntdDeCartas() {
+  while (numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
+    numCartas = Number(prompt("Deseja jogar com quantas cartas?"));
+  }
+}
+
+QntdDeCartas();
 
 
